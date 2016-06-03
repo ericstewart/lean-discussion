1f237204eb00b8fe6ffdff7b15247a8f9c8ac6d5
Eric Stewart <eric.stewart@impactivesystems.com>
2016-06-03 15:31:06 -0500

    Fixing modal submit

--- src/cljs/lean_coffee/modals.cljs
+++ src/cljs/lean_coffee/modals.cljs
@@ -6,65 +6,64 @@
             [clairvoyant.core :refer-macros [trace-forms]]
             [re-frame-tracer.core :refer [tracer]]))
 
-;; Support modal dialogs
-(def modal-id "semantic-ui-modal")
+(trace-forms {:tracer (tracer :color "gold")}
 
-(def modal-content (reagent/atom {:content [:div]}
-                          :title "Title"
-                          :actions [:div.actions]
-                          :shown nil
-                          :size nil))
+  ;; Support modal dialogs
+  (def modal-id "semantic-ui-modal")
 
-(defn get-modal []
-  (dom/getElement modal-id))
+  (def modal-content (reagent/atom {:content [:div]}
+                            :title "Title"
+                            :actions [:div.actions]
+                            :shown nil
+                            :size nil))
 
-(defn show-modal!
-  [keyboard]
-  (let [m (js/$ (get-modal))]
-    (.log js/console "In show-modal! before called show")
-    (.call (aget m "modal") m #js {:detachable false
-                                   :onDeny (:deny @modal-content)
-                                   :onApprove (:approve @modal-content)})
-    (.call (aget m "modal") m "show")
-    (.log js/console "In show-modal! after called show")))
+  (defn get-modal []
+    (dom/getElement modal-id))
+
+  (defn show-modal!
+    [keyboard]
+    (let [m (js/$ (get-modal))]
+      (.call (aget m "modal") m #js {:detachable false
+                                     :onDeny (:deny @modal-content)
+                                     :onApprove (:approve @modal-content)})
+      (.call (aget m "modal") m "show")))
 
 
-(defn close-modal! []
-  (let [m (js/$ (get-modal))]
-    (.call (aget m "modal") m "hide")))
+  (defn close-modal! []
+    (let [m (js/$ (get-modal))]
+      (.call (aget m "modal") m "hide")))
 
-(defn close-button
-  "A pre-configured close button. Just include it anywhere in the
-   modal to let the user dismiss it." []
-  [:button.close {:type "button" :data-dismiss "modal"}
-   [:span.glyphicon.glyphicon-remove {:aria-hidden "true"}]
-   [:span.sr-only "Close"]])
+  (defn close-button
+    "A pre-configured close button. Just include it anywhere in the
+     modal to let the user dismiss it." []
+    [:button.close {:type "button" :data-dismiss "modal"}
+     [:span.glyphicon.glyphicon-remove {:aria-hidden "true"}]
+     [:span.sr-only "Close"]])
 
-(defn modal-window* []
-  (let [{:keys [content title actions]} @modal-content]
-    [:div.ui.modal {:id modal-id}
-     [:div.header title]
-     [:div.content content]
-     actions]))
+  (defn modal-window* []
+    (let [{:keys [content title actions]} @modal-content]
+      [:div.ui.modal {:id modal-id}
+       [:div.header title]
+       [:div.content content]
+       actions]))
 
-(def modal-window
-  (with-meta
-    modal-window*
-    {:component-did-mount
-     (fn [e] (let [m (js/$ (get-modal))]
-               (.log js/console m)
-               (.call (aget m "on") m "onHidden"
-                      #(do (when-let [f (:hidden @modal-content)] (f))))
-                           ;(reset! modal-content {:content [:div]}))) ;;clear the modal when hidden
-               (.call (aget m "on") m "onShow"
-                      #(when-let [f (:shown @modal-content)] (f)))
-               (.call (aget m "on") m "onHide"
-                      #(when-let [f (:hide @modal-content)] (f)))))
-     :display-name "semantic modal"}))
+  (def modal-window
+    (with-meta
+      modal-window*
+      {:component-did-mount
+       (fn [e] (let [m (js/$ (get-modal))]
+                 (.call (aget m "on") m "onHidden"
+                        #(do (when-let [f (:hidden @modal-content)] (f))
+                             (reset! modal-content {:content [:div]}))) ;;clear the modal when hidden
+                 (.call (aget m "on") m "onShow"
+                        #(when-let [f (:shown @modal-content)] (f)))
+                 (.call (aget m "on") m "onHide"
+                        #(when-let [f (:hide @modal-content)] (f)))))
+       :display-name "semantic modal"}))
 
-(defn modal!
-  "Update and show the modal window."
-  ([reagent-content] (modal! reagent-content nil))
-  ([reagent-content configs]
-   (reset! modal-content (merge {:content reagent-content} configs))
-   (show-modal! (get configs :keyboard true))))
+  (defn modal!
+    "Update and show the modal window."
+    ([reagent-content] (modal! reagent-content nil))
+    ([reagent-content configs]
+     (reset! modal-content (merge {:content reagent-content} configs))
+     (show-modal! (get configs :keyboard true)))))
